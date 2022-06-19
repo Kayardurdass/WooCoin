2772d3aac1a85142d529d66d29c82ea65fe2025d
Kayardurdass <angladeulysse@gmail.com>
2022-06-19 19:22:33 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,17 +5,17 @@ const ec = new EC('secp256k1');
 const myKey = ec.keyFromPrivate('091a4cd96224e5e8e511f56a89b05a12a849fe63601efd79e3921d85be4d903c');
 const myWalletAdress = myKey.getPublic('hex');
 
-let kayardurdassCoin = new BlockChain();
+let WooCoin = new BlockChain();
 
 const tx1 = new Transaction(myWalletAdress, 'public key goes here', 10);
 tx1.signTransaction(myKey);
-kayardurdassCoin.addTransaction(tx1);
+WooCoin.addTransaction(tx1);
 
 
 console.log('\n starting the miner');
-kayardurdassCoin.minePendingTransactions(myWalletAdress);
+WooCoin.minePendingTransactions(myWalletAdress);
 
-console.log('\nBalance of kayardurdass'+kayardurdassCoin.getBalanceOfAdress(myWalletAdress));
+console.log('\nBalance of kayardurdass'+WooCoin.getBalanceOfAdress(myWalletAdress));
 
-kayardurdassCoin.minePendingTransactions(myWalletAdress);
-console.log('\nBalance of kayardurdass'+kayardurdassCoin.getBalanceOfAdress(myWalletAdress));
+WooCoin.minePendingTransactions(myWalletAdress);
+console.log('\nBalance of kayardurdass'+WooCoin.getBalanceOfAdress(myWalletAdress));
